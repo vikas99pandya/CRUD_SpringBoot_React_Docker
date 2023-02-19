ff3dd06bc5a2d1ec72fb9b3d95c80378063e663a
v.pandya <v.pandya@assai.nl>
2023-02-19 18:03:27 +0100

    added k8s configs

--- frontend/src/services/CustomerService.js
+++ frontend/src/services/CustomerService.js
@@ -1,6 +1,12 @@
 import axios from 'axios';
 
+// for cluster ip service [also do port forard from kubectl before using this service ]
+// kubectl port-forward deployment/be-deploy 8080:8080
 const CUSTOMER_API_BASE_URL = 'http://localhost:8080/api/version_1/customers';
+// for node port
+// const CUSTOMER_API_BASE_URL = 'http://192.168.15.5:30008/api/version_1/customers';
+// for Loadbalancer service [here end point is localhost, so localhost will work here]
+// const CUSTOMER_API_BASE_URL = 'http://localhost:8080/api/version_1/customers';
 class CustomerService {
 
     getAllCustomers() {
